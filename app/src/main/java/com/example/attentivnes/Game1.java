5ff8012ece3e71a2d0216a82efb863cd18c772a3
Sans <artem.chistyakov99@gmail.com>
2025-10-30 00:10:53 +0300

    First

--- app/src/main/java/com/example/attentivnes/Game1.java
+++ app/src/main/java/com/example/attentivnes/Game1.java
@@ -1,35 +1,36 @@
 package com.example.attentivnes;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.attentivnes.Models.HorizontalSpaceItemDecoration;
-import com.example.attentivnes.Models.ItemsAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
 
 public class Game1 extends AppCompatActivity {
-    RecyclerView playersRecyclerView;
-    ItemsAdapter adapter;
+    CountDownTimer timer;
     List<String> players;
     List<String> items_of_items;
     Random random = new Random();
     TextView text11;
     TextView text22;
-    int space = 0;
+    TextView countdown;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +43,16 @@ public class Game1 extends AppCompatActivity {
             return insets;
         });
 
-//        playersRecyclerView = findViewById(R.id.playersRecyclerView);
-
         items_of_items= new ArrayList<>();
         items_of_items.add("Лампа");
         items_of_items.add("Горшок");
         items_of_items.add("Подушка");
         items_of_items.add("Тарелка");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tainstvennaja_muzyka_majnkraft);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(0.8f, 0.8f);
+        mediaPlayer.start();
 
         players = new ArrayList<>();
         Add_to_list(2);
@@ -59,12 +63,44 @@ public class Game1 extends AppCompatActivity {
         text11.setText(players.get(0));
         text22.setText(players.get(1));
 
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-//        playersRecyclerView.setLayoutManager(layoutManager);
+        countdown = findViewById(R.id.TextTime);
 
-        adapter = new ItemsAdapter(players);
-//        playersRecyclerView.setAdapter(adapter);
-//        playersRecyclerView.addItemDecoration(new HorizontalSpaceItemDecoration(space));
+        timer = new CountDownTimer(180000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if (millisUntilFinished / 1000 % 60 > 10)
+                {
+                    countdown.setText("Времени осталось: " + millisUntilFinished / 1000 / 60 + ":" + millisUntilFinished / 1000 % 60);
+                }
+                else
+                {
+                    countdown.setText("Времени осталось: " + millisUntilFinished / 1000 / 60 + ":0" + millisUntilFinished / 1000 % 60);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Game1.this);
+                builder.setTitle("Время вышло!");
+                builder.setMessage("Выберите действие:");
+
+                builder.setPositiveButton("Начать заново", (dialog, which) -> {
+                    Intent intent = new Intent(Game1.this, Game1.class);
+                    startActivity(intent);
+                    finish();
+                });
+
+                builder.setNegativeButton("Выйти в главное меню", (dialog, which) -> {
+                    Intent intent = new Intent(Game1.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
+        }.start();
     }
 
     public void Add_to_list(int count) {
@@ -90,6 +126,7 @@ public class Game1 extends AppCompatActivity {
             if (players.size() == 0)
             {
                 text11.setText("");
+                win();
             }
             else
             {
@@ -103,13 +140,34 @@ public class Game1 extends AppCompatActivity {
             {
                 text22.setText(players.get(1));
             }
-            //playersRecyclerView.invalidate();
             Log.d("Чекнул", "sda");
         }
         else
         {
             Log.d("Не чек", "asd");
         }
+    }
+
+    public void win()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Game1.this);
+        builder.setTitle("Вы выиграли!");
+        builder.setMessage("Выберите действие:");
+
+        builder.setPositiveButton("Начать заново", (dialog, which) -> {
+            Intent intent = new Intent(Game1.this, Game1.class);
+            startActivity(intent);
+            finish();
+        });
+
+        builder.setNegativeButton("Выйти в главное меню", (dialog, which) -> {
+            Intent intent = new Intent(Game1.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     public void gorshok(View view) {
--- app/src/main/java/com/example/attentivnes/MainActivity.java
+++ app/src/main/java/com/example/attentivnes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.attentivnes;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button startBtn;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         startBtn = findViewById(R.id.game);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sahur);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(0.8f, 0.8f);
+        mediaPlayer.start();
     }
 
     public void start(View view) {
         Intent intent = new Intent(this, Game1.class);
         startActivity(intent);
         finish();
+        mediaPlayer.stop();
     }
 }
